d029fc06b179b8e012404ae15e931c87d4e91928
Cijams <cijams@asu.edu>
2019-01-31 19:38:50 -0800

    Unit Testing
    Started initializing and constructing unit testing to ensure proper code coverage of tests.

--- crypto_unit_testing.py
+++ crypto_unit_testing.py
@@ -3,15 +3,40 @@ import cryptomanager
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        # Create and ensure the unencrypted text is correct.
+        unencrypted_text = b'This is a test string'
+        self.assertIsNotNone(unencrypted_text)
+        print(unencrypted_text)
+
+        # Create master encryption keys
+        master_key_256 = cryptomanager.generate_master_key(1)
+        master_key_512 = cryptomanager.generate_master_key(2)
+        self.assertEqual(32, len(master_key_256))
+        self.assertEqual(64, len(master_key_512))
+
+        print("Master key 256:")
+        print(master_key_256)
+        print()
+
+        print("Master key 512:")
+        print(master_key_512)
+        print()
+
+        # Ensure key generated with the sha512 key is greater than the 256 key.
+        print(self.assertGreater(len(master_key_512), len(master_key_256)))
+
     def test_something(self):
+
         self.assertEqual(True, True)
-
-        master_key = cryptomanager.generate_master_key(1)
-
+        unencrypted_text = b'This is a test string'
+        print("\nUnencrypted text:")
+        print(unencrypted_text.decode())
+        print()
         cryptomanager.decrypt()
+        print("I RAN")
 
 
 if __name__ == '__main__':
     unittest.main()
 
-    print("k")
