e2df2382a326869ce64218b27b637f5d6146951f
Cijams <cijams@asu.edu>
2019-01-31 20:33:40 -0800

    Basic setup for unit testing.
    Starting to format.

--- crypto_unit_testing.py
+++ crypto_unit_testing.py
@@ -3,40 +3,40 @@ import cryptomanager
 
 
 class MyTestCase(unittest.TestCase):
+    master_key_256 = ""
+    master_key_512 = ""
+
     def setUp(self):
-        # Create and ensure the unencrypted text is correct.
-        unencrypted_text = b'This is a test string'
-        self.assertIsNotNone(unencrypted_text)
-        print(unencrypted_text)
+        """
+         Generate unencrypted text and master keys
+        """
+        MyTestCase.master_key_256 = cryptomanager.generate_master_key(1)
+        MyTestCase.master_key_512 = cryptomanager.generate_master_key(2)
 
-        # Create master encryption keys
-        master_key_256 = cryptomanager.generate_master_key(1)
-        master_key_512 = cryptomanager.generate_master_key(2)
-        self.assertEqual(32, len(master_key_256))
-        self.assertEqual(64, len(master_key_512))
+    def test_master_keys(self):
+        """
+          Generate unencrypted text and master keys
+         """
+        self.assertEqual(32, len(MyTestCase.master_key_256))
+        self.assertEqual(64, len(MyTestCase.master_key_512))
 
-        print("Master key 256:")
-        print(master_key_256)
-        print()
+        self.assertGreater(len(MyTestCase.master_key_512), len(MyTestCase.master_key_256))
 
-        print("Master key 512:")
-        print(master_key_512)
-        print()
+        for i in range(0, 10):
+            master_key_256 = cryptomanager.generate_master_key(1)
+            master_key_512 = cryptomanager.generate_master_key(2)
+            master_key_256_next = cryptomanager.generate_master_key(1)
+            master_key_512_next = cryptomanager.generate_master_key(2)
+            self.assertNotEqual(master_key_256, master_key_256_next)
+            self.assertNotEqual(master_key_512, master_key_512_next)
 
-        # Ensure key generated with the sha512 key is greater than the 256 key.
-        print(self.assertGreater(len(master_key_512), len(master_key_256)))
 
-    def test_something(self):
 
-        self.assertEqual(True, True)
-        unencrypted_text = b'This is a test string'
-        print("\nUnencrypted text:")
-        print(unencrypted_text.decode())
-        print()
-        cryptomanager.decrypt()
-        print("I RAN")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    case = MyTestCase()
+    case.setUp()
+    case.test_master_keys()
+
 
