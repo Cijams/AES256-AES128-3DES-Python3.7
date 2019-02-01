804e6d954ca5017dfa9c741a683419fa6b8386ba
Cijams <cijams@asu.edu>
2019-01-31 20:46:07 -0800

    Cleaned up comments
    Setting up branch.

--- crypto_unit_testing.py
+++ crypto_unit_testing.py
@@ -15,23 +15,21 @@ class MyTestCase(unittest.TestCase):
 
     def test_master_keys(self):
         """
-          Generate unencrypted text and master keys
+          Generate and test master keys
          """
+        # test keys are correct sizes
         self.assertEqual(32, len(MyTestCase.master_key_256))
         self.assertEqual(64, len(MyTestCase.master_key_512))
-
         self.assertGreater(len(MyTestCase.master_key_512), len(MyTestCase.master_key_256))
 
-        for i in range(0, 10):
+        # Test keys for uniqueness.
+        for i in range(0, 5):
             master_key_256 = cryptomanager.generate_master_key(1)
             master_key_512 = cryptomanager.generate_master_key(2)
             master_key_256_next = cryptomanager.generate_master_key(1)
             master_key_512_next = cryptomanager.generate_master_key(2)
             self.assertNotEqual(master_key_256, master_key_256_next)
             self.assertNotEqual(master_key_512, master_key_512_next)
-
-
-
 
 
 if __name__ == '__main__':
